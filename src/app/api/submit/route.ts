eb331998c3169b2f428e2c72dc086cc58d949e79
onizawashotaro4061 <shotaro10072003@gmail.com>
2024-10-26 16:42:46 +0900

    gdahi

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -5,7 +5,6 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-
 export async function POST(request: Request) {
   const body = await request.json();
   const { gender, age } = body;
