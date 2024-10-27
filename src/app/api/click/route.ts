32ab0a5fd81097a30fcd59469cfc8ebc99d52435
onizawashotaro4061 <shotaro10072003@gmail.com>
2024-10-27 15:28:21 +0900

    gdgd

--- src/app/api/click/route.ts
+++ src/app/api/click/route.ts
@@ -6,8 +6,7 @@ import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
